f4517b5cfb70a277371cc410bdee31acf3e91acd
AbbosxonDeveloper <abbosxonoripov3@gmail.com>
2023-04-24 17:23:24 +0500

    delete error changes

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -4,6 +4,7 @@ import about2 from '../../img/about2.png'
 import about3 from '../../img/about3.png'
 import about4 from '../../img/about4.png'
 import about5 from '../../img/about5.png'
+import main_about from '../../img/main_about.png'
 
 export function About() {
     return (
@@ -26,6 +27,33 @@ export function About() {
                 <div className="hero__wrapper">
                     <img className='hero__img4' src={about4}alt="about4" />
                     <img className='hero__img5' src={about5} alt="about5" />
+                </div>
+            </div>
+        </section>
+        <section className='main'>
+            <div className='main__header'>
+                <h3 className='main__header__heading'>NEVOO haqida</h3>
+                <p className='main__header__text'>
+                    Qanday targ'ib a'lo qiziquvchanlik hali urinish baxt Gey farovon taassurot ishonch bor edi Har bir kechikish uchun o'lim so'rang uslub Meni degani mumkin mening tomonidan ularda o'z ichiga olgan.
+                </p>
+            </div>
+
+            <div className='main__box'>
+                <img src={main_about} alt="main_about" />
+
+                <div className='main__wrapper'>
+
+                    <h4 className='wrapper__heading'>
+                        1000 dan ziyod talabalar o'z maqsadlariga erishish uchun biz bilan qo'shildi
+                    </h4>
+
+                    <p className='main__header__text wrapper__text'>
+                    Qanday targ'ib qilish zo'r qiziquvchanlik hali urinish baxt Gey farovon taassurot ishonchi bor edi har kechikish uchun o'lim so'rash uslub Men degani mumkin my by ularda Ekstremite endi begonalar o'z ichiga  Samimiy to'plangan mamnun olib endi abadiy nihoyatda mahrum
+                    </p>
+
+                    <p className='main__header__text wrapper__text'>
+                    Baxt farovon taassurot ishonch bor edi Har bir kechikish uchun Ularda Ekstremite endi begonalar o'z ichiga nonushta unga nutq Samimiyat yig'ilgan mamnun olib endi abadiy nihoyatda mahrum
+                    </p>
                 </div>
             </div>
         </section>
